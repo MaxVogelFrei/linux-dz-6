a5bbcd3103f38f26a89df50a5937c657d0c59390
MaxVogelFrei <madmax_001@inbox.ru>
2019-11-29 17:46:53 +0300

    dz-6

---
+++ update.log.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+linefile=/var/log/dz6/line
+logfile=/var/log/dz6/access.log
+original=/var/log/dz6/access-4560-644067.log
+rm $logfile
+cp $original $logfile
+sed -i "s/14\/Aug/$(date +%d'\/'%b)/g" $logfile
+sed -i "s/15\/Aug/$(date -d '+ 1 Day' +%d'\/'%b)/g" $logfile
+echo "1" > $linefile
